2d00f5e164de7a05deb5877fe466885d86d57014
Rostyslav Synenko <rostyslav.synenko@gmail.com>
2020-06-11 00:12:31 +0300

    Updates

---
+++ precache-manifest.81f429982e1d3a494df49140898af9cd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ea321ab7b4649b129386a3867d1d21c",
+    "url": "/meme-generator/index.html"
+  },
+  {
+    "revision": "7d68fca6966059ca9efb",
+    "url": "/meme-generator/static/css/main.ae418a22.chunk.css"
+  },
+  {
+    "revision": "f57e346ee050dc1aa3f9",
+    "url": "/meme-generator/static/js/2.7077648c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/meme-generator/static/js/2.7077648c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d68fca6966059ca9efb",
+    "url": "/meme-generator/static/js/main.f2c695e8.chunk.js"
+  },
+  {
+    "revision": "695f0e0996571106aab6",
+    "url": "/meme-generator/static/js/runtime-main.24761313.js"
+  }
+]);
